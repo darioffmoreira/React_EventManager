1a627efd0bdf89d312d8c0678bf266618c2b71f3
darioffmoreira <darioffmoreira@gmail.com>
2024-04-11 21:22:45 +0100

    add interface to return a example from props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-function MyButton() {
-  return <button className="button">My botton!</button>
+interface myBottonProps {
+  title: string
+  alt: string
+}
+
+function MyButton(props:myBottonProps) {
+  console.log(props); 
+  return <button className="button">{props.title}</button>
 }
 
 export function App() {
   return (
     <div>
-      <MyButton />
-      <MyButton />
-      <MyButton />
-      <MyButton />
-      <MyButton />
-      <MyButton />
+      <MyButton title="Click here!" alt="text" />
+      <MyButton title="text1" alt="text" />
+      <MyButton title="text2" alt="text" />
+      <MyButton title="text3" alt="text" />
       <p>oi</p>
     </div>
   )
