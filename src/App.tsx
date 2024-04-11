03d87fdaceec233c50bb6e5814e98623165161f6
darioffmoreira <darioffmoreira@gmail.com>
2024-04-11 22:56:37 +0100

    added estruture base about project

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-interface myBottonProps {
-  title: string
-  alt: string
-}
-
-function MyButton(props:myBottonProps) {
-  // console.log(props); 
-  return <button className="bg-orange-400 h-10 px-3 rounded font-medium">{props.title}</button>
-}
+import { AttendeeList } from "./components/attendes-list";
+import { Header } from "./components/header";
 
 export function App() {
   return (
     <div className="flex gap-2">
-      <MyButton title="Click here!" alt="text" />
-      <MyButton title="text1" alt="text" />
-      <MyButton title="text2" alt="text" />
-      <MyButton title="oi" alt="text" />
+      <Header />
+      <AttendeeList />
     </div>
   )
 }
