4ac947a98d0c6338576e38ab86852fd84aa203f6
darioffmoreira <darioffmoreira@gmail.com>
2024-04-14 00:08:09 +0100

    added icon toolkit «lucide-react»

--- src/components/attendes-list.tsx
+++ src/components/attendes-list.tsx
@@ -1,10 +1,11 @@
+import {Search} from 'lucide-react'
 export function AttendeeList() {
   return (
     <div className="flex items-center gap-3">
       <h1 className="text-2xl font-bold">Participants!</h1>
 
       <div className="flex items-center gap-3 px-3 w-[280px] py-1.5 border border-white/25 rounded-lg text-sm">
-        icon
+        <Search className='size-4 text-emerald-300' /> 
         <input className="bg-transparent flex-1 outline-none" placeholder="Search for participants" type="text" />
       </div>
       
