d13c9391ef4fae5dabf0ca6fe595cbeb2b8fdce9
darioffmoreira <darioffmoreira@gmail.com>
2024-04-14 00:02:29 +0100

    some styles on attendes-list component

--- src/components/attendes-list.tsx
+++ src/components/attendes-list.tsx
@@ -1,7 +1,13 @@
 export function AttendeeList() {
   return (
-    <h1>
-      Participants!
-    </h1>
+    <div className="flex items-center gap-3">
+      <h1 className="text-2xl font-bold">Participants!</h1>
+
+      <div className="flex items-center gap-3 px-3 w-[280px] py-1.5 border border-white/25 rounded-lg text-sm">
+        icon
+        <input className="bg-transparent flex-1 outline-none" placeholder="Search for participants" type="text" />
+      </div>
+      
+    </div>
   )
 }
