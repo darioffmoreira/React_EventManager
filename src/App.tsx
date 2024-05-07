e2ad800f2191d7fd018238d2a7f8b77ef9c20651
darioffmoreira <darioffmoreira@gmail.com>
2024-05-07 21:44:25 +0100

    some changes on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { AttendeeList } from "./components/attendes-list";
 import { Header } from "./components/header";
 
-export function App() {
-  return (
-    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5 p-8">
-      <Header />
-      <AttendeeList />
-    </div>
-  )
-}
+export const App = () => (
+  <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5 p-8">
+    <Header />
+
+    <AttendeeList />
+  </div>
+);
